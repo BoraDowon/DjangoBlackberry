4d6cef26d96dc1a92180e2178bcf575a2e90a0fb
akahard2dj <akahard2dj@naver.com>
2018-06-18 11:25:21 +0900

    Create models.py

---
+++ boards/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Board(models.Model):
+    title = models.CharField(max_length=1000, blank=False)
+    body = models.CharField(max_length=2000, blank=False)
+    views = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('created_at',)

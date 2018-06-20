34fe7940f7d74892bf4a808729523f4b19f24baf
akahard2dj <akahard2dj@naver.com>
2018-06-20 17:15:38 +0900

    Update views.py

--- boards/views.py
+++ boards/views.py
@@ -1,46 +1,35 @@
-from django.http import HttpResponse, JsonResponse
 from django.http import HttpRequest
-from django.views.decorators import csrf
-from django.utils.decorators import method_decorator
 
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from boards.permissions import IsBoardExists
 
 from articles.models import Article
-from articles.serializers import ArticleSerializer, ArticleListSerializer
-
-
-@csrf.csrf_exempt
-def article_list(request: HttpRequest, board_id: int):
-    if request.method == 'GET':
-        articles = Article.objects.all().filter(board_id=board_id)
-        serializer = ArticleListSerializer(articles, many=True)
-        output = dict()
-        output['data'] = serializer.data
-        output['msg'] = 'SUCCESS'
-        return JsonResponse(output, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+from articles.serializers import ArticleSerializer
+from articles.serializers import ArticleListSerializer
 
 
 class ArticleList(APIView):
-    @method_decorator(csrf.csrf_exempt)
+    permission_classes = (IsBoardExists,)
+    lookup_board_kwarg = 'board_id'
+
     def get(self, request: HttpRequest, board_id: int):
         articles = Article.objects.all().filter(board_id=board_id)
         serializer = ArticleListSerializer(articles, many=True)
         output = dict()
         output['data'] = serializer.data
         output['msg'] = 'SUCCESS'
-        return JsonResponse(output, safe=False)
+        return Response(output, status=status.HTTP_200_OK)
 
     def post(self, request: HttpRequest, board_id: int):
         data = JSONParser().parse(request)
-        article = Article(title=data['title'], body=data['body'], board_id=board_id)
-        article.save()
-        return JsonResponse({'msg': 'SUCCESS'}, status=201)
+        data[self.lookup_board_kwarg] = board_id
+
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

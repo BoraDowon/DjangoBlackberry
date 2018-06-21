0f4a90cfb23437377326a2b70b8328f847296b1c
akahard2dj <akahard2dj@naver.com>
2018-06-21 16:44:39 +0900

    Update urls.py

--- DjangoBlackberry/urls.py
+++ DjangoBlackberry/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('boards.urls')),
     path('api/v1/', include('articles.urls')),
+    path('api/v1/', include('comments.urls')),
 ]

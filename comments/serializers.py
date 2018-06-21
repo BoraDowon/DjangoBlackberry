c99d2046deaef4b4b8cc5c611b6c31c86a2d561f
akahard2dj <akahard2dj@naver.com>
2018-06-21 13:54:59 +0900

    Create serializers.py

---
+++ comments/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from comments.models import Comment
+
+
+class CommentSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    article_id = serializers.IntegerField(required=False, allow_null=False)
+    contents = serializers.CharField(required=True, allow_blank=False, max_length=1000)
+
+    updated_at = serializers.DateTimeField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return Comment.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.contents = validated_data.get('contents', instance.contents)
+        instance.save()
+        return instance

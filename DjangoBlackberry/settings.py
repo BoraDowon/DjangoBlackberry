c7c4cb021fc2e6bf0e91b4abca819c1897e7aae6
akahard2dj <akahard2dj@naver.com>
2018-06-26 17:29:43 +0900

    Update settings.py

--- DjangoBlackberry/settings.py
+++ DjangoBlackberry/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'boards.apps.BoardsConfig',
     'articles.apps.ArticlesConfig',
     'comments.apps.CommentsConfig',

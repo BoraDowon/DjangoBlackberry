f30b8bfbbbf4378420eea69cc32c3bb82547850f
akahard2dj <akahard2dj@naver.com>
2018-06-26 17:29:14 +0900

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,11 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.utils import timezone
+
+from rest_framework.authtoken.models import Token
 
 
 class BerryManager(BaseUserManager):
@@ -20,6 +25,12 @@ class BerryManager(BaseUserManager):
 
     def create_superuser(self):
         pass
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 class Berry(AbstractBaseUser):

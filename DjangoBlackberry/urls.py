4e732d8650dc7d61cd79033d5072d836707b411f
Dowon Yi <akahard2dj@192.168.nate.com>
2018-06-30 17:42:37 +0900

    libs & bug fix

--- DjangoBlackberry/urls.py
+++ DjangoBlackberry/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from DjangoBlackberry import test_view
+#from DjangoBlackberry import test_view
 from rest_framework.documentation import include_docs_urls
 from rest_framework.authtoken import views
 
